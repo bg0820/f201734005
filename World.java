2bb10df94e37f2e3d64719e06b98ac6b157851fc
Kim, Bogyu <bg0820@naver.com>
2024-10-16 09:14:40 +0900

    five

--- World.java
+++ World.java
@@ -2,4 +2,8 @@ public class World {
     public static void run() {
         System.out.println("201734005");
     }
+
+    public static void run2() {
+        System.out.println("hello world");
+    }
 }
